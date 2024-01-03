ee71919a7cd99bc9d5aa38c26e4bc98f8cff9dc4
marie-caro <marie-caroline.beck@epitech.eu>
2024-01-03 15:33:08 +0100

    CHANGE: read_file() -> no more Valgrind errors

--- lib/my/read_fill.c
+++ lib/my/read_fill.c
@@ -12,29 +12,19 @@ int my_putchar_error(char c)
     return 0;
 }
 
-int is_errno(int err, char *error)
-{
-    if (err == 2) {
-        my_putstr("error: ");
-        my_putstr(error);
-        my_putstr(": No such file or directory\n");
-        return 84;
-    }
-    return 0;
-}
-
 char **load_to_arr(char *buffer)
 {
-    int rows = 58;
+    char *tmp = my_strdup(buffer);
+    int rows = how_many_rows_str(tmp);
     char **arr = malloc(sizeof(char *) * (rows + 1));
     int i = 0;
     int j = 0;
     int k = 0;
 
-    for (; buffer[k]!= '\0'; i++) {
+    for (; tmp[k]!= '\0'; i++) {
         arr[i] = malloc(sizeof(char) * 30);
-        for (j = 0; buffer[k] != '\n'; j++) {
-            arr[i][j] = buffer[k];
+        for (j = 0; tmp[k] != '\n'; j++) {
+            arr[i][j] = tmp[k];
             k++;
         }
         arr[i][j] = '\0';
@@ -47,24 +37,20 @@ char **load_to_arr(char *buffer)
 char **read_file(int ac, char **av)
 {
     int fd = 0;
-    char buffer[30000];
-    char **arr;
+    char *buffer = malloc(sizeof(char) * 30000);
+    char **arr = malloc(sizeof(char) * 30000);
+    int bytes_read = 0;
 
     if (ac < 2) {
-        my_putstr("./my_radar: bad arguments: 0 given but 84 is required");
-        my_putstr("\nretry with -h\n");
+        print_error();
         return NULL;
     }
+    memset(buffer, 0, sizeof(char) * 30000);
     for (int i = 1; i < ac; i++) {
         fd = open(av[i], O_RDONLY);
-        if (errno != 1) {
-            is_errno(errno , av[i]);
-            return NULL;
-        }
-        read(fd, buffer, 30000);
+        bytes_read = read(fd, buffer,30000);
         arr = load_to_arr(buffer);
         close(fd);
     }
-    my_printf("\n");
     return arr;
 }
